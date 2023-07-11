dc37fdb0de6fef0f233941435ef27d044eaf346b
juanmarab <juanmanueluruguay21@gmail.com>
2023-07-11 18:47:01 -0300

    WIP Menu

---
+++ Assets/Scrpits/PauseScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ContinueGameScene()
+    {
+        
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
